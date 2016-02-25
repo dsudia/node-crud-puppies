f69e711a21ce24eecb6088349472ea80f1c55e54
David Sudia <dsudia@gmail.com>
2016-02-25 14:10:57 -0700

    add app.get and pg

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var connectionString = 'postgres://localhost:5432/puppies';
+
+app.get('/api/puppies', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+
+    if(err) {
+      res.send('Error fetching Client: ' + err);
+      done();
+    }
+
+    client.query('select * from puppies', function(err, result) {
+      return result;
+    });
+
+  });
+});
+
+function getAllPuppies(client) {
+  return client.query('select * from puppies', function(err, result) {
+    return result;
+  });
+}
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
